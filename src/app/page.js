6111b65b284901b1709a2451e978526329dcc870
gauravgangwarweb <gauravgangwar501@gmail.com>
2025-02-17 19:45:53 +0530

    Remove unused AboutDes, AboutTrader, and Hero components along with associated assets

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,7 @@
-import Image from "next/image";
-import Hero from "./components/hero/Hero";
-import AboutDes from "./components/about-des/AboutDes";
-import AboutTrader from "./components/about-trader/AboutTrader";
-
 export default function Home() {
   return (
     <div className="w-full md:max-w-[1440px] mx-auto container">
-      <Hero />
-      <AboutDes />
-      <AboutTrader />
+        
     </div>
   );
 }
